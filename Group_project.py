4b9cf50e12c5ec6cf42028087a5fdf5f92ff2373
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-20 12:22:25 +0100

    Code to choose variables of interest and writing a csv file

--- Group_project.py
+++ Group_project.py
@@ -2,7 +2,23 @@
 
 #Loading json files
 import json
+# import csv
 
 # Loading the json data
-with open('A_people.json', encoding='utf8') as file:
-    data = json.load(file) # gives you a list of dictionaries
+
+letters = 'ABCDEFG' 
+for letter in letters: #selects every json file in the folder
+    filename = f'{letter}_people.json'
+
+    with open(filename, encoding='utf8') as file:
+        data = json.load(file) #gives you a list of dictionaries
+
+        with open('people.csv', 'a', encoding='utf8') as file: #writing csv file opening function
+            for datum in data: #choose whatever we want to look for
+                if "ontology/country_label" in datum and "ontology/birthYear" in datum:
+                    if datum["ontology/country_label"] == "United Kingdom" and datum["ontology/birthYear"] == "1944":
+                        print(datum["title"])
+
+                        file.write(f'{datum["title"]}\n') #select what you want to put in the csv file
+          
+        

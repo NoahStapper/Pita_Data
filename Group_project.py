1a4dab7f3e9fbbca64550c9e1b49e3284c8a85cb
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-20 15:20:06 +0100

    Finding country from multiple search conditions (nationality, dob, etc)

--- Group_project.py
+++ Group_project.py
@@ -4,9 +4,13 @@
 import json
 # import csv
 
+# variable names
+keys_potential_country = ["ontology/country_label", "ontology/nationality_label"]
+country = "United Kingdom"
+
 # Loading the json data
 
-letters = 'ABCDEFG' #remember to add the rest of the letters at the start of json files
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' #remember to add the rest of the letters at the start of json files
 for letter in letters: #selects every json file in the folder
     filename = f'{letter}_people.json'
 
@@ -15,10 +19,20 @@ for letter in letters: #selects every json file in the folder
 
         with open('people.csv', 'a', encoding='utf8') as file: #writing csv file opening function
             for datum in data: #choose whatever we want to look for
-                if "ontology/country_label" in datum and "ontology/deathCause_label" in datum:
-                    if datum["ontology/country_label"] == "United Kingdom": #and datum["ontology/deathCause_label"] == "suicide":
-                        print(datum["ontology/deathCause_label"])
+                for key in keys_potential_country:
+                    if key in datum and country in datum[key]: #If label is existing key, and corresponding value is country name, add to results list
+                        if "ontology/deathCause_label" in datum:
+                            print(datum["ontology/deathCause_label"])
+                            #for label in labels:
 
-                        file.write(f'{datum["ontology/deathCause_label"]}\n') #select what you want to put in the csv file
+                        break
+                            
+                
+               # if "ontology/country_label" in datum or "ontology/nationality_label" in datum: # and "ontology/deathCause_label" in datum
+                #    if datum["ontology/country_label"] == "United Kingdom" or datum["ontology/nationality_label"] == "United Kingdom": #and datum["ontology/deathCause_label"] == "suicide":
+                 #       print(datum["ontology/deathCause_label"])
+                    #print(datum["ontology/country_label"] or datum["ontology/nationality_label"])
+
+                        file.write(f'{datum[label]}\n') #select what you want to put in the csv file
           
         

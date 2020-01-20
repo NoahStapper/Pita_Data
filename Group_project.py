8ff1f442c428b4dbe6353de11188517c993d3305
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-20 10:17:14 +0100

    Loading in json files (A_people.json)

--- Group_project.py
+++ Group_project.py
@@ -1,1 +1,8 @@
 #Welcome Pita data people
+
+#Loading json files
+import json
+
+# Loading the json data
+with open('A_people.json', encoding='utf8') as file:
+    data = json.load(file) # gives you a list of dictionaries
